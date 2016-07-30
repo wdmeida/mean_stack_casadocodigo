deb6c6af728499ab49fb3fcd56874f43eb976d1f
wdmeida <wdmeida@gmail.com>
2016-07-30 15:04:45 -0300

    Criação da listagem de contatos.

--- cotatooh/config/express.js
+++ cotatooh/config/express.js
@@ -2,6 +2,8 @@
 var express = require('express');
 //Importa o módulo passando como parâmetro a instância configurada do Express.
 var home = require('../app/routes/home');
+//Importa o módulo do Express-load responsável por gerenciar o carregamento de módulos.
+var load = require('express-load');
 
 module.exports = function() {
   var app = express();
@@ -14,7 +16,22 @@ module.exports = function() {
   app.set('view engine', 'ejs');
   app.set('views', './app/views');
 
-  home(app);
+  /*
+    A função load carregará todos os scripts dentro das pastas app/models, app/controllers
+    e app/routes. No final, a função into adiciona dinamicamente na instância do Express
+    propriedades que apontam para esses módulos.
+
+    Um ponto importante é que precisamos carregar as pastas seguindo a ordem models, controllers
+    e routes, caso contrário não conseguiremos, por exemplo, ter acesso aos nossos controllers
+    em nossas rotas caso os módulos com nossos controllers tenham sido carregados por último.
+
+    O parâmetro {cwd: 'app'} foi necessário para mudar o diretório padrão, pois a função procura
+    as pastas no diretório raiz contatooh e precisamos que ela considere a pasta contatooh/app.
+  */
+  load('models', {cwd: 'app'})
+      .then('controllers')
+      .then('routes')
+      .into(app);
 
   return app;
 };
