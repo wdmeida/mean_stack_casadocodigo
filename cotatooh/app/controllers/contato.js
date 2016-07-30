58445c1f4c37fd5e72f4c46c5887823c1ebf5908
wdmeida <wdmeida@gmail.com>
2016-07-30 18:13:11 -0300

    Inclusão da listagem de contatos de forma individual.

--- cotatooh/app/controllers/contato.js
+++ cotatooh/app/controllers/contato.js
@@ -9,9 +9,20 @@ var contatos = [
 
 module.exports = function() {
   var controller = {};
+
   controller.listaContatos = function(req, res) {
     //envia a lista no formato json.
     res.json(contatos);
   };
+
+  controller.obtemContato = function(req, res) {
+    var idContato = req.params.id; //Obtém o id do contato requisitado.
+    var contato = contatos.filter(function(contato) {
+      return contato._id == idContato;
+    })[0];
+    //Verifica se contato foi encontrado, caso seja, retorna um json com o conteúdo.
+    contato ? res.json(contato) : res.status(404).send('Contato não encontrado');
+  };
+
   return controller;
 };
--- cotatooh/config/express.js
+++ cotatooh/config/express.js
@@ -4,6 +4,7 @@ var express = require('express');
 var home = require('../app/routes/home');
 //Importa o módulo do Express-load responsável por gerenciar o carregamento de módulos.
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function() {
   var app = express();
@@ -11,6 +12,15 @@ module.exports = function() {
   app.set('port', 3000);
   //  middleware
   app.use(express.static('./public'));
+
+  /*
+    Solicita ao bodyParser que realize o parse de json e requisições com o corpo
+    x-ww-form-urlencoded. Isso permite acessar os dados da requisição através de
+    req.body, algo que veremos na prática mais tarde.
+  */
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(require('method-override')());
 
   // Define o template engine que sera usado para gerar as views e onde ficarão armazenadas.
   app.set('view engine', 'ejs');
